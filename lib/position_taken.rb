2e3d7a9c312961d04119f5c82feed0714961dd6b
Paulina Salem <paulinasalem@yahoo.com>
2018-04-02 22:39:04 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken?(board, index)
-if array[index_number] == " "
+if board[index_number] == " "
   return false
-elsif array[index_number] == ""
+elsif board[index_number] == ""
   return false
 end
 end
